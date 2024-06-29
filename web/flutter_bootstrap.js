0ec253f3885d97838c753aeb50957a5dead6c6bf
_juliotati <59662912+Juliotati@users.noreply.github.com>
2024-06-29 10:17:58 +0100

    chore: update web app initialization

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,18 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+window.addEventListener('load', function (ev) {
+    // Download main.dart.js
+    _flutter.loader.loadEntrypoint({
+        serviceWorker: {
+            serviceWorkerVersion: {{flutter_service_worker_version}},
+        },
+        onEntrypointLoaded: function (engineInitializer) {
+            engineInitializer.initializeEngine({
+                useColorEmoji: true,
+            }).then(function (appRunner) {
+                appRunner.runApp();
+            });
+        }
+    });
+});
